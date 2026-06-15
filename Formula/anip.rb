8422a3da6d6c64bf908c0f332c4e8f9639d43c67
Samir Al-Latifi <samirski@users.noreply.github.com>
2026-06-15 02:24:28 -0700

    Update anip formula to v0.24.6

--- Formula/anip.rb
+++ Formula/anip.rb
@@ -1,26 +1,26 @@
 class Anip < Formula
   desc "ANIP contract generator and validator CLI"
   homepage "https://anip.dev"
-  version "0.24.5"
+  version "0.24.6"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/anip-protocol/anip/releases/download/v0.24.5/anip_0.24.5_darwin_arm64.tar.gz"
-      sha256 "9662877bf62039e09b76f420d266bb5bb8e0347fbcea89feb42e6cb2b44208be"
+      url "https://github.com/anip-protocol/anip/releases/download/v0.24.6/anip_0.24.6_darwin_arm64.tar.gz"
+      sha256 "32dbff28fc92e95c79ab4bc3cbbc84b94abef2459d45a117ee2323161eb4dd7e"
     else
-      url "https://github.com/anip-protocol/anip/releases/download/v0.24.5/anip_0.24.5_darwin_amd64.tar.gz"
-      sha256 "4a9492179d233366116ed9235046d3670afb79846a433b7de7f90113a9655e66"
+      url "https://github.com/anip-protocol/anip/releases/download/v0.24.6/anip_0.24.6_darwin_amd64.tar.gz"
+      sha256 "c8685c78d08b6ebac4ec8ea0e2871c98b1ffd44e1a030fd62c504512dd6158c7"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/anip-protocol/anip/releases/download/v0.24.5/anip_0.24.5_linux_arm64.tar.gz"
-      sha256 "6cda320ee5e9e3947a97b4ed8107ed8c609f376632fc346d03184aaf9596cc62"
+      url "https://github.com/anip-protocol/anip/releases/download/v0.24.6/anip_0.24.6_linux_arm64.tar.gz"
+      sha256 "1b0c7e5f17a9ebe0ed3c3b3f71a6df3e6ab78f5fcd361a6068f8917ba433abd1"
     else
-      url "https://github.com/anip-protocol/anip/releases/download/v0.24.5/anip_0.24.5_linux_amd64.tar.gz"
-      sha256 "7bd21f0afa9a1928010fab2f1f09962e520d714fb959b7b92b29b24db3e3544f"
+      url "https://github.com/anip-protocol/anip/releases/download/v0.24.6/anip_0.24.6_linux_amd64.tar.gz"
+      sha256 "394d63543ffa822fef4c37b75722df36f58597fd7070ba5ef887b9838c8378bb"
     end
   end
 
